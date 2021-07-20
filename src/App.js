352170716e1555fdd43e2d96caa249a8bad8c69d
Xuan Huan Ninh <huannx@mintpotvn.com>
2021-07-20 08:34:54 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SlideshowGallery from './components/slideshow-gallery/slideshow-gallery';
+import img1 from './images/01.jpg';
+import img2 from './images/02.jpg';
+import img3 from './images/03.jpg';
+import img4 from './images/04.jpg';
+import img5 from './images/05.jpg';
+import img6 from './images/06.jpg';
 
-export default App;
+const collection = [
+    {src: img1, caption: "Caption one"},
+    {src: img2, caption: "Caption two"},
+    {src: img3, caption: "Caption three"},
+    {src: img4, caption: "Caption four"},
+    {src: img5, caption: "Caption five"},
+    {src: img6, caption: "Caption six"},
+];
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <SlideshowGallery
+                    input={collection}
+                    ratio={`3:2`}
+                    mode={`manual`}
+                />
+
+                <SlideshowGallery
+                    input={collection}
+                    ratio={`3:2`}
+                    mode={`automatic`}
+                    timeout={`3000`}
+                />
+
+                <div>
+                    Made by <a href="https://about.phamvanlam.com/">Lam Pham</a>.
+                    Visit me at <a href="/">completejavascript.com</a>.
+                </div>
+            </div>
+        );
+    }
+}
